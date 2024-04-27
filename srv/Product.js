f203df1bd5cbb9c94fb195200d49e19e98eaed39
raoshrinivas <165559014+raoshrinivas@users.noreply.github.com>
2024-04-27 11:59:11 +0530

    Update Product.js
    New Line Added

--- srv/Product.js
+++ srv/Product.js
@@ -4,4 +4,5 @@ module.exports = (helloword) => {
   //helloword.on('display', req => `${req.data.input1} word` )
     
    helloword.on('addition', ({data:{input1,input2}}) => input1+input2) 
+   helloword.on('subtraction', ({data:{input1,input2}}) => input1-input2)
 }
